bd7bd74e450c83c48cea591ed08391673f5406f4
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 17:05:37 +0000

    Created basic API Server.

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const port = 3100;
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json("Welcome to API Server!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started in port ${port}!`);
+});
