b971e2b4ca7b50a501cf482cf1e7ee13da20dae5
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 15:01:17 +0000

    Implemented a Header Component and added styles.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
 
 export default class App extends Component {
   render() {
-    return <div className="App">Hello!</div>;
+    return (
+      <div className="App">
+        <Header dark={true} className="Header">
+          GfG Notes App
+        </Header>
+      </div>
+    );
   }
 }
