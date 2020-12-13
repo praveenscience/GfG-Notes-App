1aa0f57225bc361e0b0c3600bd6694a7af48e1ab
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 15:42:20 +0000

    Created a Login form.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,13 @@
 import React from "react";
+import LoginForm from "./_LoginForm";
 
 const Login = ({ handleAuth }) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-6">Login</div>
+        <div className="col-6">
+          <LoginForm handleAuth={handleAuth} />
+        </div>
         <div className="col-6">Register</div>
       </div>
     </div>
---
+++ client/src/components/Login/_LoginForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import FormGroup from "../Forms/FormGroup";
+
+class LoginForm extends Component {
+  state = {
+    Username: "",
+    Password: ""
+  };
+  handleChange = e => {
+    console.log(`${e.target.name}: ${e.target.value}`);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleAuth(this.state.Username, this.state.Password);
+  };
+  render() {
+    return (
+      <form className="LoginForm" onSubmit={this.handleSubmit}>
+        <h2>Login</h2>
+        {[
+          {
+            Id: "Username",
+            Type: "text",
+            Desc: "Put your username in right case."
+          },
+          {
+            Id: "Password",
+            Type: "password",
+            Desc: "Put your password that you might have forgotten"
+          }
+        ].map((fg, key) => (
+          <FormGroup
+            {...fg}
+            Value={this.state[fg.Id]}
+            Label={fg.Id}
+            key={key}
+            onChange={this.handleChange}
+          />
+        ))}
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
