ae38f897348ba4931ea203d20ed13788ee7696aa
Praveen Kumar <praveenscience@gmail.com>
2020-12-13 17:10:44 +0000

    Implemented Express Sessions.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const session = require("express-session");
 const api = require("./routes/api");
 const app = express();
 const port = 3100;
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
 
 app.get("/", (req, res) => {
   res.json("Welcome to API Server!");
