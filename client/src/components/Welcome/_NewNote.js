8b0a151ff6c5a993b6198ae7af0567a520d2f61a
Praveen Kumar <praveenscience@gmail.com>
2020-12-19 17:41:56 +0000

    Implemented handleSubmit and handleReset for new note.

--- client/src/components/Welcome/_NewNote.js
+++ client/src/components/Welcome/_NewNote.js
@@ -7,8 +7,17 @@ const NewNote = ({ User }) => {
   const [Desc, setDesc] = useState("");
   const [Title, setTitle] = useState("");
   const NoteID = Sluggify(Title);
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log({ NoteID, Desc, Title, User });
+  };
+  const handleReset = e => {
+    e.preventDefault();
+    setDesc("");
+    setTitle("");
+  };
   return (
-    <div className="NewNote">
+    <form className="NewNote" onSubmit={handleSubmit} onReset={handleReset}>
       <h3 className="mb-3">
         Creating
         <input
@@ -22,11 +31,21 @@ const NewNote = ({ User }) => {
         <em>Being created by {User}...</em>
       </p>
       <MDEditor
-        height={window.innerHeight - 300}
+        height={window.innerHeight - 350}
         value={Desc}
         onChange={setDesc}
       />
-    </div>
+      <input
+        type="submit"
+        value="Create Note"
+        className="btn btn-primary mt-3"
+      />
+      <input
+        type="reset"
+        value="Reset"
+        className="btn btn-outline-secondary mt-3 ml-3"
+      />
+    </form>
   );
 };
 
