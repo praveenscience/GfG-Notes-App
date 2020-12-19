c5b1e4bb9f024c76818ddc637e4e7e1b75ddffbf
Praveen Kumar <praveenscience@gmail.com>
2020-12-19 16:33:11 +0000

    Added Bhooshan's Note.

--- server/routes/note.js
+++ server/routes/note.js
@@ -22,6 +22,13 @@ const Notes = [
     Desc:
       "Biff Howard Tannen is the main antagonist of the first 2 films, and a local bully who harassed George McFly and managed to alter history in the second film. He comes from a long line of bullies in Hill Valley, most of whom harassed members of the McFly family. Biff is also a descendant of Buford Tannen (also portrayed by Wilson), who is one of Hill Valley's outlaws during the 1880s.",
     User: "Praveen"
+  },
+  {
+    NoteID: "ned",
+    Title: "Ned Stark",
+    Desc:
+      "Eddard Stark is a fictional character in the 1996 fantasy novel A Game of Thrones by George R. R. Martin, and Game of Thrones, HBO's adaptation of Martin's A Song of Ice and Fire series. In the storyline, Ned is the lord of Winterfell, an ancient fortress in the North of the fictional continent of Westeros.",
+    User: "Bhooshan"
   }
 ];
 
