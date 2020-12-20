8c02b5d8415ef377498fc2683a8d819c597df2e0
Praveen Kumar <praveenscience@gmail.com>
2020-12-20 16:35:47 +0000

    Created an endpoint for file uploads.

--- server/routes/file.js
+++ server/routes/file.js
@@ -3,5 +3,8 @@ const file = require("express").Router();
 file.get("/", (req, res) => {
   res.json("Files Endpoint!");
 });
+file.post("/", (req, res) => {
+  res.json(Object.keys(req));
+});
 
 module.exports = file;
