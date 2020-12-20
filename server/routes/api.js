4af3b746ae4b2e12cfa0a1fee2dd081b84dc6a49
Bhooshan <me.45.birjebhooshan@gmail.com>
2020-12-20 20:58:56 +0530

    Made the table born for users

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,27 @@ const user = require("./user");
 app.get("/", (req, res) => {
   res.json("Welcome to /api!");
 });
+
+app.get("/seed", (req, res) => {
+  const db = req.app.get("db");
+  db.schema.hasTable("users").then(exists => {
+    if (!exists) {
+      db.schema
+        .createTable("users", table => {
+          table.increments("id").primary();
+          table.string("name");
+          table.string("pass");
+          table.timestamp("born").defaultTo(db.fn.now());
+        })
+        .then(() => {
+          res.status(201).json("Table just born");
+        });
+    } else {
+      res.json("Table already born");
+    }
+  });
+});
+
 app.use("/note", note);
 app.use("/user", user);
 
