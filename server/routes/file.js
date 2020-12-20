d86bf4c3a90e12360425dcc050078bd017254601
Praveen Kumar <praveenscience@gmail.com>
2020-12-20 16:48:57 +0000

    Implemented file uploads.

--- server/routes/file.js
+++ server/routes/file.js
@@ -1,10 +1,14 @@
 const file = require("express").Router();
+const multer = require("multer");
+const upload = multer({
+  dest: __dirname.replace("/routes", "") + "/uploads"
+});
 
 file.get("/", (req, res) => {
   res.json("Files Endpoint!");
 });
-file.post("/", (req, res) => {
-  res.json(Object.keys(req));
+file.post("/", upload.single("file"), (req, res) => {
+  res.json(req.file);
 });
 
 module.exports = file;
